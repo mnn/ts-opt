10e546455d4794bf84cefab89735a25b3c805a90
monnef <mkucera22@gmail.com>
2019-03-16 18:57:42 +0100

    Changed target to ES5.

--- dist/src/Opt.js
+++ dist/src/Opt.js
@@ -1,7 +1,24 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @typeparam T Wrapped value type.
  */
-export class Opt {
+var Opt = /** @class */ (function () {
+    function Opt() {
+    }
     /**
      * Create Opt instance from an array of one or zero items.
      *
@@ -12,16 +29,20 @@ export class Opt {
      *
      * @param x
      */
-    static fromArray(x) { return opt(x[0]); }
-    /**
-     * `1` for [[Some]], `0` for [[None]].
-     */
-    get length() { return this.isEmpty ? 0 : 1; }
+    Opt.fromArray = function (x) { return exports.opt(x[0]); };
+    Object.defineProperty(Opt.prototype, "length", {
+        /**
+         * `1` for [[Some]], `0` for [[None]].
+         */
+        get: function () { return this.isEmpty ? 0 : 1; },
+        enumerable: true,
+        configurable: true
+    });
     /**
      * Alias of [[flatMap]]
      * @param f
      */
-    chain(f) { return this.flatMap(f); }
+    Opt.prototype.chain = function (f) { return this.flatMap(f); };
     /**
      * Combination of [[flatMap]] and [[opt]] functions.
      *
@@ -32,7 +53,7 @@ export class Opt {
      *
      * @param f
      */
-    chainToOpt(f) { return this.flatMap(x => opt(f(x))); }
+    Opt.prototype.chainToOpt = function (f) { return this.flatMap(function (x) { return exports.opt(f(x)); }); };
     /**
      * Applies passed function to this instance and returns function result.
      * Also known as a function application, `|>` or pipe operator.
@@ -44,80 +65,98 @@ export class Opt {
      *
      * @param f
      */
-    pipe(f) { return f(this); }
-}
-class None extends Opt {
-    toArray() { return []; }
-    get isEmpty() { return true; }
-    flatMap(_f) { return none; }
-    map() { return none; }
-    orCrash(msg) { throw new Error(msg); }
-    orNull() { return null; }
-    orUndef() { return undefined; }
-    caseOf(_onSome, onNone) {
+    Opt.prototype.pipe = function (f) { return f(this); };
+    return Opt;
+}());
+exports.Opt = Opt;
+var None = /** @class */ (function (_super) {
+    __extends(None, _super);
+    function None() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    None.prototype.toArray = function () { return []; };
+    Object.defineProperty(None.prototype, "isEmpty", {
+        get: function () { return true; },
+        enumerable: true,
+        configurable: true
+    });
+    None.prototype.flatMap = function (_f) { return exports.none; };
+    None.prototype.map = function () { return exports.none; };
+    None.prototype.orCrash = function (msg) { throw new Error(msg); };
+    None.prototype.orNull = function () { return null; };
+    None.prototype.orUndef = function () { return undefined; };
+    None.prototype.caseOf = function (_onSome, onNone) {
         return onNone();
+    };
+    None.prototype.onNone = function (f) { f(); };
+    None.prototype.onSome = function (_f) { };
+    None.prototype.contains = function (_x) { return false; };
+    None.prototype.exists = function (_p) { return false; };
+    None.prototype.forAll = function (_p) { return true; };
+    None.prototype.orElse = function (def) { return def; };
+    None.prototype.orElseOpt = function (def) { return def; };
+    None.prototype.bimap = function (_someF, noneF) { return exports.opt(noneF()); };
+    None.prototype.flatBimap = function (_someF, noneF) { return noneF(); };
+    None.prototype.toString = function () { return 'None'; };
+    return None;
+}(Opt));
+var Some = /** @class */ (function (_super) {
+    __extends(Some, _super);
+    function Some(value) {
+        var _this = _super.call(this) || this;
+        _this.value = value;
+        return _this;
     }
-    onNone(f) { f(); }
-    onSome(_f) { }
-    contains(_x) { return false; }
-    exists(_p) { return false; }
-    forAll(_p) { return true; }
-    orElse(def) { return def; }
-    orElseOpt(def) { return def; }
-    bimap(_someF, noneF) { return opt(noneF()); }
-    flatBimap(_someF, noneF) { return noneF(); }
-    toString() { return 'None'; }
-}
-class Some extends Opt {
-    constructor(value) {
-        super();
-        this.value = value;
-    }
-    toArray() { return [this.value]; }
-    get isEmpty() { return false; }
-    flatMap(f) {
+    Some.prototype.toArray = function () { return [this.value]; };
+    Object.defineProperty(Some.prototype, "isEmpty", {
+        get: function () { return false; },
+        enumerable: true,
+        configurable: true
+    });
+    Some.prototype.flatMap = function (f) {
         return f(this.value);
-    }
-    map(f) {
+    };
+    Some.prototype.map = function (f) {
         return new Some(f(this.value));
-    }
-    orCrash(_msg) { return this.value; }
-    orNull() { return this.value; }
-    orUndef() { return this.value; }
-    caseOf(onSome, _onNone) { return onSome(this.value); }
-    contains(x) { return this.value === x; }
-    exists(p) { return p(this.value); }
-    forAll(p) { return p(this.value); }
-    onNone(_f) { }
-    onSome(f) { f(this.value); }
-    orElse(_def) { return this.value; }
-    orElseOpt(_def) { return this; }
-    bimap(someF, _noneF) { return opt(someF(this.value)); }
-    flatBimap(someF, _noneF) { return someF(this.value); }
-    toString() { return `Some(${JSON.stringify(this.value)})`; }
-}
-const isNoneValue = (x) => {
+    };
+    Some.prototype.orCrash = function (_msg) { return this.value; };
+    Some.prototype.orNull = function () { return this.value; };
+    Some.prototype.orUndef = function () { return this.value; };
+    Some.prototype.caseOf = function (onSome, _onNone) { return onSome(this.value); };
+    Some.prototype.contains = function (x) { return this.value === x; };
+    Some.prototype.exists = function (p) { return p(this.value); };
+    Some.prototype.forAll = function (p) { return p(this.value); };
+    Some.prototype.onNone = function (_f) { };
+    Some.prototype.onSome = function (f) { f(this.value); };
+    Some.prototype.orElse = function (_def) { return this.value; };
+    Some.prototype.orElseOpt = function (_def) { return this; };
+    Some.prototype.bimap = function (someF, _noneF) { return exports.opt(someF(this.value)); };
+    Some.prototype.flatBimap = function (someF, _noneF) { return someF(this.value); };
+    Some.prototype.toString = function () { return "Some(" + JSON.stringify(this.value) + ")"; };
+    return Some;
+}(Opt));
+var isNoneValue = function (x) {
     return x === undefined || x === null || Number.isNaN(x);
 };
 /**
  * Single global instance of [[None]].
  */
-export const none = Object.freeze(new None());
+exports.none = Object.freeze(new None());
 /**
  * Constructs [[Some]].
  * Usually it is [[opt]] you are looking for (only in rare cases you want to have for example `Some(undefined)`).
  * @param x
  */
-export const some = (x) => new Some(x);
+exports.some = function (x) { return new Some(x); };
 /**
  * Main constructor function - for `undefined`, `null` and `NaN` returns [[None]].
  * Anything else is wrapped into [[Some]].
  * @param x
  */
-export const opt = (x) => isNoneValue(x) ? none : new Some(x);
+exports.opt = function (x) { return isNoneValue(x) ? exports.none : new Some(x); };
 /**
  * Is given value an instance of [[Opt]]?
  * @param x
  */
-export const isOpt = (x) => x instanceof Opt;
+exports.isOpt = function (x) { return x instanceof Opt; };
 //# sourceMappingURL=Opt.js.map
--- dist/test/Opt.js
+++ dist/test/Opt.js
@@ -1,201 +1,203 @@
-import * as chai from 'chai';
-import * as spies from 'chai-spies';
-import { opt, some, none, isOpt, Opt } from '../src/Opt';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var chai = require("chai");
+var spies = require("chai-spies");
+var Opt_1 = require("../src/Opt");
 chai.use(spies);
-const { expect } = chai;
+var expect = chai.expect;
 chai.should();
-const add1 = (x) => x + 1;
-const randomNumOpt = () => Math.random() > .5 ? none : some(Math.random());
-describe('opt', () => {
-    it('construction', () => {
-        expect(opt(undefined).isEmpty).to.be.true;
-        expect(opt(NaN).isEmpty).to.be.true;
-        expect(opt(null).isEmpty).to.be.true;
-        expect(opt('').isEmpty).to.be.false;
-        expect(opt('a').isEmpty).to.be.false;
-        expect(opt(0).isEmpty).to.be.false;
-        expect(opt(1).isEmpty).to.be.false;
-        expect(opt({}).isEmpty).to.be.false;
-        expect(opt([]).isEmpty).to.be.false;
+var add1 = function (x) { return x + 1; };
+var randomNumOpt = function () { return Math.random() > .5 ? Opt_1.none : Opt_1.some(Math.random()); };
+describe('opt', function () {
+    it('construction', function () {
+        expect(Opt_1.opt(undefined).isEmpty).to.be.true;
+        expect(Opt_1.opt(NaN).isEmpty).to.be.true;
+        expect(Opt_1.opt(null).isEmpty).to.be.true;
+        expect(Opt_1.opt('').isEmpty).to.be.false;
+        expect(Opt_1.opt('a').isEmpty).to.be.false;
+        expect(Opt_1.opt(0).isEmpty).to.be.false;
+        expect(Opt_1.opt(1).isEmpty).to.be.false;
+        expect(Opt_1.opt({}).isEmpty).to.be.false;
+        expect(Opt_1.opt([]).isEmpty).to.be.false;
     });
-    it('length', () => {
-        expect(opt(null).length).to.eq(0);
-        expect(opt(0).length).to.eq(1);
+    it('length', function () {
+        expect(Opt_1.opt(null).length).to.eq(0);
+        expect(Opt_1.opt(0).length).to.eq(1);
     });
-    it('orUndef', () => {
-        expect(opt(null).orUndef()).to.eq(undefined);
-        expect(opt(0).orUndef()).to.eq(0);
+    it('orUndef', function () {
+        expect(Opt_1.opt(null).orUndef()).to.eq(undefined);
+        expect(Opt_1.opt(0).orUndef()).to.eq(0);
     });
-    it('orNull', () => {
-        expect(opt(null).orNull()).to.eq(null);
-        expect(opt(0).orNull()).to.eq(0);
+    it('orNull', function () {
+        expect(Opt_1.opt(null).orNull()).to.eq(null);
+        expect(Opt_1.opt(0).orNull()).to.eq(0);
     });
-    it('orCrash', () => {
-        expect(() => opt(null).orCrash('')).to.throw();
-        expect(opt(0).orCrash('')).to.eq(0);
-        expect(opt([]).orCrash('')).to.eql([]);
+    it('orCrash', function () {
+        expect(function () { return Opt_1.opt(null).orCrash(''); }).to.throw();
+        expect(Opt_1.opt(0).orCrash('')).to.eq(0);
+        expect(Opt_1.opt([]).orCrash('')).to.eql([]);
     });
-    it('map', () => {
-        expect(opt(1).map(add1).orNull()).to.eq(2);
-        expect(opt(null).map(add1).orUndef()).to.eq(undefined);
+    it('map', function () {
+        expect(Opt_1.opt(1).map(add1).orNull()).to.eq(2);
+        expect(Opt_1.opt(null).map(add1).orUndef()).to.eq(undefined);
     });
-    it('flatMap', () => {
-        expect(opt(1).flatMap(() => some(2)).orNull()).to.eq(2);
-        expect(opt(1).flatMap(() => none).orNull()).to.eq(null);
-        expect(opt(null).flatMap(() => none).orUndef()).to.eq(undefined);
-        expect(opt(null).chain(() => none).orUndef()).to.eq(undefined);
+    it('flatMap', function () {
+        expect(Opt_1.opt(1).flatMap(function () { return Opt_1.some(2); }).orNull()).to.eq(2);
+        expect(Opt_1.opt(1).flatMap(function () { return Opt_1.none; }).orNull()).to.eq(null);
+        expect(Opt_1.opt(null).flatMap(function () { return Opt_1.none; }).orUndef()).to.eq(undefined);
+        expect(Opt_1.opt(null).chain(function () { return Opt_1.none; }).orUndef()).to.eq(undefined);
     });
-    it('toArray', () => {
-        expect(opt(1).toArray()).to.eql([1]);
-        expect(opt(null).toArray()).to.eql([]);
+    it('toArray', function () {
+        expect(Opt_1.opt(1).toArray()).to.eql([1]);
+        expect(Opt_1.opt(null).toArray()).to.eql([]);
     });
-    describe('caseOf', () => {
-        it('fp', () => {
-            expect(some(1).caseOf(x => 'x' + x, () => 'y')).to.eql('x1');
-            expect(none.caseOf(x => 'x' + x, () => 'y')).to.eql('y');
-            expect(randomNumOpt().caseOf(() => 'x', () => 'y')).to.be.oneOf(['x', 'y']);
+    describe('caseOf', function () {
+        it('fp', function () {
+            expect(Opt_1.some(1).caseOf(function (x) { return 'x' + x; }, function () { return 'y'; })).to.eql('x1');
+            expect(Opt_1.none.caseOf(function (x) { return 'x' + x; }, function () { return 'y'; })).to.eql('y');
+            expect(randomNumOpt().caseOf(function () { return 'x'; }, function () { return 'y'; })).to.be.oneOf(['x', 'y']);
         });
-        it('imperative', () => {
-            const noneCb = chai.spy();
-            const someCb = chai.spy();
+        it('imperative', function () {
+            var noneCb = chai.spy();
+            var someCb = chai.spy();
             noneCb.should.have.not.been.called();
             someCb.should.have.not.been.called();
-            none.caseOf(someCb, noneCb);
+            Opt_1.none.caseOf(someCb, noneCb);
             someCb.should.have.not.been.called();
             noneCb.should.have.been.called.once;
-            some(1).caseOf(someCb, noneCb);
+            Opt_1.some(1).caseOf(someCb, noneCb);
             someCb.should.have.been.called.once;
             someCb.should.have.been.called.always.with(1);
             noneCb.should.have.been.called.once;
         });
     });
-    it('pipe', () => {
-        expect(some(1).pipe(isOpt)).to.be.true;
-        expect(none.pipe(isOpt)).to.be.true;
+    it('pipe', function () {
+        expect(Opt_1.some(1).pipe(Opt_1.isOpt)).to.be.true;
+        expect(Opt_1.none.pipe(Opt_1.isOpt)).to.be.true;
     });
-    it('onNone', () => {
-        const cb = chai.spy();
+    it('onNone', function () {
+        var cb = chai.spy();
         cb.should.have.not.been.called();
-        some(1).onNone(cb);
+        Opt_1.some(1).onNone(cb);
         cb.should.have.not.been.called();
-        none.onNone(cb);
+        Opt_1.none.onNone(cb);
         cb.should.have.been.called.once;
     });
-    it('onSome', () => {
-        const cb = chai.spy();
+    it('onSome', function () {
+        var cb = chai.spy();
         cb.should.have.not.been.called();
-        none.onSome(cb);
+        Opt_1.none.onSome(cb);
         cb.should.have.not.been.called();
-        some(1).onSome(cb);
+        Opt_1.some(1).onSome(cb);
         cb.should.have.been.called.once;
         cb.should.have.been.called.with.exactly(1);
     });
-    it('contains', () => {
-        expect(none.contains(0)).to.be.false;
-        expect(some(1).contains(0)).to.be.false;
-        expect(some(0).contains(0)).to.be.true;
+    it('contains', function () {
+        expect(Opt_1.none.contains(0)).to.be.false;
+        expect(Opt_1.some(1).contains(0)).to.be.false;
+        expect(Opt_1.some(0).contains(0)).to.be.true;
     });
-    it('exists', () => {
-        expect(none.exists(() => true)).to.be.false;
-        expect(none.exists(() => false)).to.be.false;
-        expect(some(0).exists(() => false)).to.be.false;
-        expect(some(0).exists(() => true)).to.be.true;
-        const cb = chai.spy(() => true);
-        none.exists(cb);
+    it('exists', function () {
+        expect(Opt_1.none.exists(function () { return true; })).to.be.false;
+        expect(Opt_1.none.exists(function () { return false; })).to.be.false;
+        expect(Opt_1.some(0).exists(function () { return false; })).to.be.false;
+        expect(Opt_1.some(0).exists(function () { return true; })).to.be.true;
+        var cb = chai.spy(function () { return true; });
+        Opt_1.none.exists(cb);
         cb.should.have.not.been.called();
-        some(0).exists(cb);
+        Opt_1.some(0).exists(cb);
         cb.should.have.been.called.with.exactly(0);
     });
-    it('forAll', () => {
-        expect(none.forAll(() => true)).to.be.true;
-        expect(none.forAll(() => false)).to.be.true;
-        expect(some(1).forAll(() => true)).to.be.true;
-        expect(some(1).forAll(() => false)).to.be.false;
-        const cb = chai.spy(() => true);
-        none.forAll(cb);
+    it('forAll', function () {
+        expect(Opt_1.none.forAll(function () { return true; })).to.be.true;
+        expect(Opt_1.none.forAll(function () { return false; })).to.be.true;
+        expect(Opt_1.some(1).forAll(function () { return true; })).to.be.true;
+        expect(Opt_1.some(1).forAll(function () { return false; })).to.be.false;
+        var cb = chai.spy(function () { return true; });
+        Opt_1.none.forAll(cb);
         cb.should.have.not.been.called();
-        some(0).forAll(cb);
+        Opt_1.some(0).forAll(cb);
         cb.should.have.been.called.with.exactly(0);
     });
-    it('orElse', () => {
-        expect(none.orElse(0)).to.be.eq(0);
-        expect(some(1).orElse(0)).to.be.eq(1);
+    it('orElse', function () {
+        expect(Opt_1.none.orElse(0)).to.be.eq(0);
+        expect(Opt_1.some(1).orElse(0)).to.be.eq(1);
     });
-    it('orElseOpt', () => {
-        expect(none.orElseOpt(some(0)).orNull()).to.be.eq(0);
-        expect(none.orElseOpt(none).orNull()).to.be.eq(null);
-        expect(some(1).orElseOpt(none).orNull()).to.be.eq(1);
+    it('orElseOpt', function () {
+        expect(Opt_1.none.orElseOpt(Opt_1.some(0)).orNull()).to.be.eq(0);
+        expect(Opt_1.none.orElseOpt(Opt_1.none).orNull()).to.be.eq(null);
+        expect(Opt_1.some(1).orElseOpt(Opt_1.none).orNull()).to.be.eq(1);
     });
-    it('bimap', () => {
-        expect(none.bimap(x => x, () => 'none').orNull()).to.be.eq('none');
-        expect(some('some').bimap(x => x, () => 'none').orNull()).to.be.eq('some');
+    it('bimap', function () {
+        expect(Opt_1.none.bimap(function (x) { return x; }, function () { return 'none'; }).orNull()).to.be.eq('none');
+        expect(Opt_1.some('some').bimap(function (x) { return x; }, function () { return 'none'; }).orNull()).to.be.eq('some');
     });
-    it('flatBimap', () => {
-        expect(none.flatBimap(x => some(x), () => some('none')).orNull()).to.be.eq('none');
-        expect(some('some').flatBimap(x => some(x), () => some('none')).orNull()).to.be.eq('some');
+    it('flatBimap', function () {
+        expect(Opt_1.none.flatBimap(function (x) { return Opt_1.some(x); }, function () { return Opt_1.some('none'); }).orNull()).to.be.eq('none');
+        expect(Opt_1.some('some').flatBimap(function (x) { return Opt_1.some(x); }, function () { return Opt_1.some('none'); }).orNull()).to.be.eq('some');
     });
-    it('toString', () => {
-        expect(some(1).toString()).to.eq('Some(1)');
-        expect(some('a').toString()).to.eq('Some("a")');
-        expect(some(null).toString()).to.eq('Some(null)');
-        expect(none.toString()).to.eq('None');
+    it('toString', function () {
+        expect(Opt_1.some(1).toString()).to.eq('Some(1)');
+        expect(Opt_1.some('a').toString()).to.eq('Some("a")');
+        expect(Opt_1.some(null).toString()).to.eq('Some(null)');
+        expect(Opt_1.none.toString()).to.eq('None');
     });
-    it('chainToOpt', () => {
-        expect(some(1).chainToOpt(x => x === 1 ? null : x + 1).orUndef()).to.be.eq(undefined);
-        expect(some(2).chainToOpt(x => x === 1 ? null : x + 1).orUndef()).to.be.eq(3);
+    it('chainToOpt', function () {
+        expect(Opt_1.some(1).chainToOpt(function (x) { return x === 1 ? null : x + 1; }).orUndef()).to.be.eq(undefined);
+        expect(Opt_1.some(2).chainToOpt(function (x) { return x === 1 ? null : x + 1; }).orUndef()).to.be.eq(3);
     });
 });
-describe('helper functions', () => {
-    describe('some', () => {
-        it('create', () => {
-            expect(some(0).orNull()).to.eq(0);
-            expect(some('').orNull()).to.eq('');
-            expect(some(NaN).orNull()).to.eql(NaN);
-            expect(some(null).orUndef()).to.eq(null);
+describe('helper functions', function () {
+    describe('some', function () {
+        it('create', function () {
+            expect(Opt_1.some(0).orNull()).to.eq(0);
+            expect(Opt_1.some('').orNull()).to.eq('');
+            expect(Opt_1.some(NaN).orNull()).to.eql(NaN);
+            expect(Opt_1.some(null).orUndef()).to.eq(null);
         });
-        it('falsy', () => {
-            expect(some(null).orUndef()).to.eq(null);
-            expect(some(NaN).orUndef()).to.eql(NaN);
-            expect(some(undefined).orNull()).to.eq(undefined);
+        it('falsy', function () {
+            expect(Opt_1.some(null).orUndef()).to.eq(null);
+            expect(Opt_1.some(NaN).orUndef()).to.eql(NaN);
+            expect(Opt_1.some(undefined).orNull()).to.eq(undefined);
         });
     });
-    it('fromArray', () => {
-        expect(Opt.fromArray([]).orNull()).to.eq(null);
-        expect(Opt.fromArray([1]).orNull()).to.eq(1);
+    it('fromArray', function () {
+        expect(Opt_1.Opt.fromArray([]).orNull()).to.eq(null);
+        expect(Opt_1.Opt.fromArray([1]).orNull()).to.eq(1);
         // expect(fromArray([1, 2]).orNull()).to.eq(1);
     });
-    it('isOpt', () => {
-        expect(isOpt(0)).to.be.false;
-        expect(isOpt(undefined)).to.be.false;
-        expect(isOpt({ value: 0 })).to.be.false;
-        expect(isOpt(some(1))).to.be.true;
-        expect(isOpt(none)).to.be.true;
+    it('isOpt', function () {
+        expect(Opt_1.isOpt(0)).to.be.false;
+        expect(Opt_1.isOpt(undefined)).to.be.false;
+        expect(Opt_1.isOpt({ value: 0 })).to.be.false;
+        expect(Opt_1.isOpt(Opt_1.some(1))).to.be.true;
+        expect(Opt_1.isOpt(Opt_1.none)).to.be.true;
     });
 });
-describe('examples', () => {
-    it('1', () => {
-        const db = {
+describe('examples', function () {
+    it('1', function () {
+        var db = {
             '0': { name: 'John', surname: null },
             '1': { name: 'Worf', surname: 'Mercer' }
         };
-        const capitalize = (x) => x.split('').map(y => y.toUpperCase()).join('');
+        var capitalize = function (x) { return x.split('').map(function (y) { return y.toUpperCase(); }).join(''); };
         // without
-        const f = (id) => {
+        var f = function (id) {
             if (id === undefined) {
                 return null;
             }
-            const item = db[id];
+            var item = db[id];
             if (!item) {
                 return null;
             }
-            const surname = item.surname ? capitalize(item.surname) : '<missing>';
+            var surname = item.surname ? capitalize(item.surname) : '<missing>';
             return item.name + ' ' + surname;
         };
         // with
-        const g = (id) => opt(id)
-            .chainToOpt(x => db[x])
-            .map(item => item.name + ' ' + opt(item.surname).map(capitalize).orElse('<missing>'))
-            .orNull();
+        var g = function (id) { return Opt_1.opt(id)
+            .chainToOpt(function (x) { return db[x]; })
+            .map(function (item) { return item.name + ' ' + Opt_1.opt(item.surname).map(capitalize).orElse('<missing>'); })
+            .orNull(); };
         f(0); // 'John <missing>'
         g(0); // 'John <missing>'
         f(1); // 'Worf MERCER'
